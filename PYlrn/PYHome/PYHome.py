a099a8691e24fbb944fd71eb0075a674905cec5b
slotnet <amitbisht007@gmail.com>
2017-11-02 22:46:55 +0530

    updated

--- PYlrn/PYHome/PYHome.py
+++ PYlrn/PYHome/PYHome.py
@@ -1,10 +1,1 @@
-﻿import numpy as np     # installed with matplotlib
-import matplotlib.pyplot as plt
-from math import radians
-
-def main():  
-    x = np.arange(0, radians(1800), radians(12))
-    plt.plot(x, np.cos(x), 'b')
-    plt.show()
-
-main()
+﻿print("Hi! start now")
